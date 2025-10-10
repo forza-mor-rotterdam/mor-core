711f1540cec6cef7663f9a4b14b054f085f47184
Maurice Guikema <maurice@mgui.nl>
2025-10-10 10:04:55 +0200

    fix: buurten & wijken

--- app/apps/locatie/viewsets.py
+++ app/apps/locatie/viewsets.py
@@ -2,13 +2,11 @@ from apps.locatie.models import Locatie
 from apps.locatie.serializers import BuurtWijkSerializer
 from config.context import db
 from django.conf import settings
-from django.db.models import Q
+from django.db import connection
 from drf_spectacular.utils import extend_schema
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
-# from django.db import connection
 
 
 def dictfetchall(cursor):
@@ -41,24 +39,40 @@ class LocatieViewSet(
     )
     def buurten_met_wijken(self, request):
         with db(settings.READONLY_DATABASE_KEY):
-            queryset = (
-                self.filter_queryset(self.get_queryset())
-                .filter(
-                    buurtnaam__isnull=False,
-                    wijknaam__isnull=False,
-                    plaatsnaam__isnull=False,
-                )
-                .exclude(Q(buurtnaam="") | Q(wijknaam="") | Q(plaatsnaam=""))
-                .values("buurtnaam", "wijknaam", "plaatsnaam")
-                .distinct()
-                .order_by("plaatsnaam", "wijknaam", "buurtnaam")
-            )
+            # queryset = (
+            #     self.filter_queryset(self.get_queryset())
+            #     .exclude(
+            #         Q(buurtnaam="", buurtnaam__isnull=True) |
+            #         Q(wijknaam="", wijknaam__isnull=True) |
+            #         Q(plaatsnaam="", plaatsnaam__isnull=True)
+            #     )
+            #     .values("buurtnaam", "wijknaam", "plaatsnaam")
+            #     .distinct()
+            #     .order_by("plaatsnaam", "wijknaam", "buurtnaam")
+            # )
 
-            # raw_sql_distinct = 'SELECT DISTINCT "locatie_locatie"."buurtnaam", "locatie_locatie"."wijknaam", "locatie_locatie"."plaatsnaam" FROM "locatie_locatie" WHERE ("locatie_locatie"."buurtnaam" IS NOT NULL AND "locatie_locatie"."plaatsnaam" IS NOT NULL AND "locatie_locatie"."wijknaam" IS NOT NULL) ORDER BY "locatie_locatie"."plaatsnaam" ASC, "locatie_locatie"."wijknaam" ASC, "locatie_locatie"."buurtnaam" ASC'
-            # raw_sql = 'SELECT "locatie_locatie"."buurtnaam", "locatie_locatie"."wijknaam", "locatie_locatie"."plaatsnaam" FROM "locatie_locatie" WHERE ("locatie_locatie"."buurtnaam" IS NOT NULL AND "locatie_locatie"."plaatsnaam" IS NOT NULL AND "locatie_locatie"."wijknaam" IS NOT NULL) GROUP BY ("locatie_locatie"."buurtnaam", "locatie_locatie"."wijknaam", "locatie_locatie"."plaatsnaam") ORDER BY "locatie_locatie"."plaatsnaam" ASC, "locatie_locatie"."wijknaam" ASC, "locatie_locatie"."buurtnaam" ASC'
-            # with connection.cursor() as c:
-            #     c.execute(raw_sql)
-            #     queryset = dictfetchall(c)
+            raw_sql = '\
+                SELECT DISTINCT \
+                    "locatie_locatie"."buurtnaam", \
+                    "locatie_locatie"."wijknaam", \
+                    "locatie_locatie"."plaatsnaam" \
+                FROM "locatie_locatie" \
+                WHERE \
+                    "locatie_locatie"."buurtnaam" IS NOT NULL \
+                    AND "locatie_locatie"."plaatsnaam" IS NOT NULL \
+                    AND "locatie_locatie"."wijknaam" IS NOT NULL \
+                    AND ("locatie_locatie"."buurtnaam" = \'\') IS NOT TRUE \
+                    AND ("locatie_locatie"."wijknaam" = \'\') IS NOT TRUE \
+                    AND ("locatie_locatie"."plaatsnaam" = \'\') IS NOT TRUE \
+                ORDER BY \
+                    "locatie_locatie"."buurtnaam" ASC, \
+                    "locatie_locatie"."wijknaam" ASC, \
+                    "locatie_locatie"."plaatsnaam" ASC \
+            '
+
+            with connection.cursor() as c:
+                c.execute(raw_sql)
+                queryset = dictfetchall(c)
 
             return Response(
                 BuurtWijkSerializer(
