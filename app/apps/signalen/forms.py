753452f6d1e6f9279cc3e8edd7158f76c44c24d4
Maurice Guikema <maurice@mgui.nl>
2026-02-11 18:03:53 +0100

    update signaal_url

--- app/apps/signalen/forms.py
+++ app/apps/signalen/forms.py
@@ -3,6 +3,7 @@ from django import forms
 
 class UpdateSignaalSignaalUrlForm(forms.Form):
     csv_file = forms.FileField()
+    bron_id = forms.CharField(initial="MeldR", required=False)
     url_prefix = forms.CharField(required=False)
     dryrun = forms.BooleanField(initial=True, required=False)
     trailing_slash = forms.BooleanField(initial=False, required=False)
